266a5d6e46d2bf8a6f9b9bc1a41ac735e11d5ad9
Ubuntu <ubuntu@ip-172-26-7-20.ap-south-1.compute.internal>
2026-02-10 04:39:12 +0000

    Clean commit - Flask Docker App

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+import psycopg2
+import os
+import time
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host=os.environ.get("POSTGRES_HOST"),
+        database=os.environ.get("POSTGRES_DB"),
+        user=os.environ.get("POSTGRES_USER"),
+        password=os.environ.get("POSTGRES_PASSWORD")
+    )
+    return conn
+
+@app.route("/")
+def home():
+    try:
+        conn = get_db_connection()
+        conn.close()
+        return "Flask + PostgreSQL is connected successfully!"
+    except Exception as e:
+        return f"Database connection failed: {e}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
